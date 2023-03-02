00a761abcc6fb960609fdaf56e86da5baa762dd1
Vamsi Sam <chandravamsi31@gmail.com>
2023-03-02 19:35:09 +0530

    UserList component creation

---
+++ src/app/users-list/users-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent {
+  listOfUSers: any = [];
+
+  
+}
