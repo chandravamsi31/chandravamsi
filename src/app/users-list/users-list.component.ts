a39fd433b780f439011d376cdaca98c06c381bc7
Vamsi Sam <chandravamsi31@gmail.com>
2023-03-04 19:14:10 +0530

    interface topic

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,29 +1,55 @@
 import { Component } from '@angular/core';
-
-@Component({
+export interface userStructure {
+  id: number;
+  name: string;
+  age?: any;
+}
+@Component({  
   selector: 'users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent {
-  newUser: any = "";
-  listOfUSers: any[] = [
-    "Suma",
-    "Jyo",
-    "pavani",
-    "CV",
-    "sasi",
-    "sai charan",
-    "niranjan",
-    "movayya",
-    "sandeep",
-    "yeswanth",
-    "management",
-    "bhargavi",
-    "sai"
+  newUser: any = "10";
+  userPwd: any = "";
+  listOfStudent: userStructure[] = [
+    {
+      "name": "sasi",
+      "id": 18,
+      "age": 70
+
+    },
+    {
+      "id": 19,
+      "name": "CV",
+      "age": 50
+    },
+    {
+      "id": 20,
+      "name": "Pavani",
+      "age": "23" 
+    },
+    {
+      "id": 21,
+      "name": "Malleswari",
+      "age": 100
+    },
+    {
+      "id": 22,
+      "name": "Jyothi"
+    },
+    {
+      "id": 23,
+      "name": "Sai",
+      "age": 110
+    }
   ];
 
+  constructor() {
+    console.log(this.listOfStudent);
+  }
+
   addUser() {
-    this.listOfUSers.unshift(this.newUser);
+    // this.listOfStudent.push(this.newUser);
   }
 }
